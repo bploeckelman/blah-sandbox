0b3cb120fb4ee00bf9b3c9e1792fa4f8706fdf1a
Brian Ploeckelman <brian.ploeckelman@gmail.com>
2021-01-13 00:21:38 -0600

    Add player start state and beam in animation

--- src/components/player.cpp
+++ src/components/player.cpp
@@ -64,8 +64,18 @@ void Player::update() {
         anim->scale.x = Calc::abs(anim->scale.x) * m_facing;
     }
 
+    // START STATE
+    if (m_state == st_start) {
+        anim->play("beam-in");
+        m_start_timer += Time::delta;
+        if (m_start_timer >= anim->animation()->duration()) {
+            m_start_timer = 0;
+            m_state = st_normal;
+            anim->play("idle");
+        }
+    }
     // NORMAL STATE
-    if (m_state == st_normal) {
+    else if (m_state == st_normal) {
         // current animation
         if (m_on_ground) {
             if (input != 0) {
--- src/components/player.h
+++ src/components/player.h
@@ -13,6 +13,7 @@ namespace Zen {
         static constexpr int st_normal = 0;
         static constexpr int st_attack = 1;
         static constexpr int st_hurt   = 2;
+        static constexpr int st_start  = 3;
 
         int health = 3;
 
@@ -25,12 +26,13 @@ namespace Zen {
         void update() override;
 
     private:
-        int m_state = st_normal;
+        int m_state = st_start;
         int m_facing = 1;
         float m_jump_timer = 0;
         float m_attack_timer = 0;
         float m_hurt_timer = 0;
         float m_invincible_timer = 0;
+        float m_start_timer = 0;
         float m_on_ground = false;
         Collider* m_attack_collider = nullptr;
 
